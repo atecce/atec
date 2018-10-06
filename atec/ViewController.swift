9f1f2328cee3bb550a4871e43e557c46a143916f
atec <root@atec.pub>
2018-10-06 13:52:30 -0400

    Initial Commit

---
+++ atec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  atec
+//
+//  Created by Alex Tecce on 10/6/18.
+//  Copyright © 2018 telos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
